0ef4b8844883dc8d42a88fc9a7ebeec9b90e0dbe
Bhavya Joshi <jshi.bhavya@gmail.com>
2022-08-01 23:29:52 +0530

    Changes

--- configs/database/dbConnection.js
+++ configs/database/dbConnection.js
@@ -5,8 +5,6 @@ mongoose.connect(process.env.MONGO_URL + process.env.DATABASE,{
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
-console.log(process.env.DATABASE);
-console.log(process.env.MONGO_URL);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
